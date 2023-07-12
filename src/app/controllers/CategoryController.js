7cf92a212ec2aaa3b6fa01b26e449386194c4dd3
DaniMendess <danimendes9728@gmail.com>
2023-07-11 21:16:26 -0300

    update of products and update of category

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -23,6 +23,8 @@ class CategoryController {
 
     const { name } = request.body
 
+    const { filename: path } = request.file
+
     const categoryExists = await Category.findOne({
       where: {
         name
@@ -34,7 +36,8 @@ class CategoryController {
     }
 
     const { id } = await Category.create({
-      name
+      name,
+      path
     })
 
     return response.json({ id, name })
@@ -43,6 +46,51 @@ class CategoryController {
   async index (request, response) {
     const category = await Category.findAll()
     return response.json(category)
+  }
+
+  async update (request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required()
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { name } = request.body
+
+    const { id } = request.params
+
+    const category = await Category.findByPk(id)
+
+    if (!category) {
+      return response.status(401).json({ error: 'Category not indetified' })
+    }
+
+    let path
+
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    await Category.update(
+      {
+        name,
+        path
+      }, {
+        where: { id }
+      }
+    )
+
+    return response.status(200).json()
   }
 }
 
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -9,7 +9,8 @@ class ProductsController {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
       price: Yup.number().required(),
-      category_id: Yup.number().required()
+      category_id: Yup.number().required(),
+      offer: Yup.boolean()
     })
 
     try {
@@ -25,13 +26,15 @@ class ProductsController {
     }
 
     const { filename: path } = request.file
-    const { name, price, category_id } = request.body
+
+    const { name, price, category_id, offer } = request.body
 
     const product = await Product.create({
       name,
       price,
       category_id,
-      path
+      path,
+      offer
     })
 
     return response.json(product)
@@ -48,6 +51,59 @@ class ProductsController {
       ]
     })
     return response.json(products)
+  }
+
+  async update (request, response) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      price: Yup.number(),
+      category_id: Yup.number(),
+      offer: Yup.boolean()
+    })
+
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json()
+    }
+
+    const { id } = request.params
+
+    const product = await Product.findByPk(id)
+
+    if (!product) {
+      return response.status(401).json({ error: 'Product not found' })
+    }
+
+    let path
+
+    if (request.file) {
+      path = request.file.filename
+    }
+
+    const { name, price, category_id, offer } = request.body
+
+    await Product.update(
+      {
+        name,
+        price,
+        category_id,
+        path,
+        offer
+      },
+      {
+        where: { id }
+      }
+
+    )
+
+    return response.status(200).json()
   }
 }
 
